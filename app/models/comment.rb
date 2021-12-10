83aae89d5f0fa0328945cf0ef52f3a65f135db76
anonymous anonymous <jacqueline.hong@kellogg.northwestern.edu>
2021-12-10 19:10:32 +0000

    Direct association between poets and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :commenter,
+             :class_name => "Poet"
+
   belongs_to :poem
 
   # Indirect associations
--- app/models/poet.rb
+++ app/models/poet.rb
@@ -1,6 +1,10 @@
 class Poet < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :foreign_key => "commenter_id",
+             :dependent => :destroy
+
   has_many   :poems,
              :foreign_key => "author_id",
              :dependent => :destroy
