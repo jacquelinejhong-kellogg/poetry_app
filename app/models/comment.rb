8d4ca0d62cf9670853de546203a48cbbcc030fdd
anonymous anonymous <jacqueline.hong@kellogg.northwestern.edu>
2021-12-10 19:10:32 +0000

    Direct association between poems and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :poem
+
   # Indirect associations
 
   # Validations
--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,6 +1,9 @@
 class Poem < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
