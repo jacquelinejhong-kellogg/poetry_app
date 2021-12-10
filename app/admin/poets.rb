a7b1aa907c138b6a1c271cda10ab0936476fb7cb
anonymous anonymous <jacqueline.hong@kellogg.northwestern.edu>
2021-12-10 19:10:41 +0000

    Whitelist columns in Poet for ActiveAdmin

--- app/admin/poets.rb
+++ app/admin/poets.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Poet do
 
+ permit_params :email, :password, :name, :bio, :dob, :photo
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
